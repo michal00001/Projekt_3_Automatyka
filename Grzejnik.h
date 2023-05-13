e36530828af4a5b3a73ed97c9939d8670bf42bb9
michal00001 <michal.grzeskowiak2001@gmail.com>
2023-05-13 15:06:54 +0200

    Etap 1-dodano klasy Symulacja i Grzejnik [utworzono metody wg schematu UML]

---
+++ Grzejnik.h
@@ -0,0 +1,7 @@
+#pragma once
+class Grzejnik
+{
+	float moc_nominalna; //[W]
+	float poziom_mocy; //[%]
+};
+
---
+++ Symulacja.h
@@ -0,0 +1,9 @@
+#pragma once
+class Symulacja
+{
+public:
+	void iteracja(float _zmienna);
+	void przebieg(int _zmienna1, float _zmienna2);
+	void zapis(char* _nazwa);
+};
+
